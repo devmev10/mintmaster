32a5544f67415419f14dc6430ac402a04d6640d4
Ravi Tokas <ravitokas90@gmail.com>
2023-09-06 11:07:34 +0530

    Add Listing struct and counters

--- contracts/marketplace.sol
+++ contracts/marketplace.sol
@@ -7,3 +7,27 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
 contract Marketplace is ReentrancyGuard, Ownable {
+    using Counters for Counters.Counter;
+    Counters.Counter private marketplaceIds;
+    Counters.Counter private totalMarketplaceItemsSold;
+
+    mapping(uint => Listing) private marketplaceIdToListingItem;
+
+    struct Listing {
+        uint marketplaceId;
+        address nftAddress;
+        uint tokenId;
+        address payable seller;
+        address payable owner;
+        uint listPrice;
+    }
+
+    event ListingCreated(
+        uint indexed marketplaceId,
+        address indexed nftAddress,
+        uint indexed tokenId,
+        address seller,
+        address owner,
+        uint listPrice
+    );
+}
